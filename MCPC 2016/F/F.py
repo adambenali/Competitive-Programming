c922d5c0d772d75ab733be734c31f6422a33a718
AdamBenali <adaambenali@gmail.com>
2017-10-30 14:45:19 +0100

    Problem F

---
+++ MCPC 2016/F/F.py
@@ -0,0 +1,14 @@
+for _ in range(int(input()):
+    line = input().split()
+    x, y = int(line[0]), int(line[1])
+    s = line[2]
+    if s == ">":
+        if x > y:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        if x < y:
+            print("Yes")
+        else:
+            print("No")
